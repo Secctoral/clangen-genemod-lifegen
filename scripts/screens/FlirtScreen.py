53ad30529889afdbbade9432b03fc36f959fd163
Secctoral <secodont@gmail.com>
2024-10-28 19:08:54 -0700

    Update FlirtScreen.py

--- scripts/screens/FlirtScreen.py
+++ scripts/screens/FlirtScreen.py
@@ -102,7 +102,7 @@ class FlirtScreen(Screens):
             )
         # self.textbox_graphic.hide()
 
-        self.profile_elements["cat_image"] = pygame_gui.elements.UIImageui_((35, 450), (200, 200))),
+        self.profile_elements["cat_image"] = pygame_gui.elements.UIImageui_(((35, 450), (200, 200)),
                                                                         pygame.transform.scale(
                                                                             generate_sprite(self.the_cat),
                                                                             (200, 200)), manager=MANAGER)
@@ -303,7 +303,7 @@ class FlirtScreen(Screens):
             text = text[0]
 
             #the background image for the text
-            option_bg = pygame_gui.elements.UIImageui_((430, 428 + y_pos), (270, 35))),
+            option_bg = pygame_gui.elements.UIImageui_(((430, 428 + y_pos), (270, 35)),
                                                             pygame.transform.scale(
                                                                 image_cache.load_image(
                                                                     "resources/images/option_bg.png").convert_alpha(),
@@ -311,7 +311,7 @@ class FlirtScreen(Screens):
             self.option_bgs[c] = option_bg
 
             #the button for dialogue choices
-            button = UIImageButtonui_((390, 428 + y_pos), (34, 34))),
+            button = UIImageButtonui_(((390, 428 + y_pos), (34, 34)),
                                         text = "",
                                         object_id="#dialogue_choice_button", manager=MANAGER)
             self.choice_buttons[c] = button
@@ -319,7 +319,7 @@ class FlirtScreen(Screens):
 
             #the text for dialogue choices
             option = pygame_gui.elements.UITextBox(str(text),
-                                                            ui_scale(pygame.Rect((428, 430 + y_pos), (270, 30))),
+                                                            ui_scale(pygame.Rect(((428, 430 + y_pos), (270, 30)),
                                                             object_id="#text_box_30_horizleft",
                                                             manager=MANAGER)
             self.text_choices[c] = option
